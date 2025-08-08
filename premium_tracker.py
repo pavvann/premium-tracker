42e231a2f9e67bbb978e7c461956f7155af19a71
Pawan <pawanbajaj2012@gmail.com>
2025-08-09 00:36:35 +0530

    plot things

--- premium_tracker.py
+++ premium_tracker.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def get_binance_klines(symbol, interval, limit):
     url = "https://api.binance.com/api/v3/klines"
@@ -56,3 +57,42 @@ df_merged["basis"] = df_merged["close_perp"] - df_merged["close_spot"]
 df_merged["basis_pct"] = (df_merged["basis"] / df_merged["close_spot"]) * 100
 
 print(df_merged.head())
+
+df = df_merged.copy()
+df['time'] = pd.to_datetime(df["time"])
+
+static_threshold = 0.05
+
+mean_pct = df['basis_pct'].mean()
+std_pct = df["basis_pct"].std()
+upper_dyn = mean_pct + 2*std_pct
+lower_dyn = mean_pct - 2*std_pct
+
+sig_up = df[df["basis_pct"] > static_threshold]
+sig_down = df[df["basis_pct"] < -static_threshold]
+
+fig, ax = plt.subplots(figsize=(12,5))
+ax.plot(df['time'], df["basis_pct"], label="Basis (%)", linewidth = 1)
+
+# static threshold lines
+ax.axhline(static_threshold, linestyle=':', linewidth=1, label=f'Static +{static_threshold}%')
+ax.axhline(-static_threshold, linestyle=':', linewidth=1, label=f'Static -{static_threshold}%')
+
+
+# dynaminc threshold lines
+ax.axhline(upper_dyn, linestyle='--', linewidth=1, label=f'Dyn upper (mean + 2sigma) = {upper_dyn: .4f}%')
+ax.axhline(lower_dyn, linestyle='--', linewidth=1, label=f'Dyn lower (mean - 2sigma) = {lower_dyn: .4f}%')
+
+ax.scatter(sig_up['time'], sig_up['basis_pct'], marker='^', s = 70, label='Static > +0.05%')
+ax.scatter(sig_down['time'], sig_down['basis_pct'], marker='v', s = 70, label='Static < -0.05%')
+
+
+ax.set_title("BTC Perpetual vs Spot - Basis (%)")
+ax.set_xlabel('Date')
+ax.set_ylabel('Basis (%)')
+ax.legend(loc='best')
+plt.xticks(rotation=25)
+plt.tight_layout()
+plt.show()
+
+
